368023afc0104cead00ac28cf795dc4c7cd0ca82
Reece Lee <reecelee0128@gmail.com>
2025-05-20 13:21:18 -1000

    Add click functionality with my item components

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,11 +1,26 @@
 function ListGroup() {
-    return <ul className="list-group">
-  <li className="list-group-item">An item</li>
-  <li className="list-group-item">A second item</li>
-  <li className="list-group-item">A third item</li>
-  <li className="list-group-item">A fourth item</li>
-  <li className="list-group-item">And a fifth one</li>
-</ul>;
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  const getMessage = () => {
+    return items.length === 0 ? <p>No item found</p> : null;
+  };
+  return (
+    <>
+      <h1>List Group</h1>
+      {getMessage}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={() => console.log(item)}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default ListGroup;
