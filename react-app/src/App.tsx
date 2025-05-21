b4d9320a636e7b3533b9d370e7dd164f885ca1c2
Reece Lee <reecelee0128@gmail.com>
2025-05-20 22:30:16 -1000

    Add button component

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,5 +1,6 @@
 import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
+import Button from "./components/Button";
 
 function App() {
   const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
@@ -9,16 +10,17 @@ function App() {
   };
   return (
     <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      ></ListGroup>
       <Alert>Hello World</Alert>
+      <Button></Button>
     </div>
   );
 }
 
 export default App;
-/*
-<ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      ></ListGroup>
-*/
+
+//
---
+++ react-app/src/components/Button.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Button() {
+  return <div className="btn btn-primary">Button</div>;
+}
