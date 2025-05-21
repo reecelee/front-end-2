2192dbcb56427612570a3c9e163328f37f4dba5a
Reece Lee <reecelee0128@gmail.com>
2025-05-20 21:47:10 -1000

    Add state functionality

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,19 +1,25 @@
+import { useState } from "react";
+
 function ListGroup() {
   const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
-  const getMessage = () => {
-    return items.length === 0 ? <p>No item found</p> : null;
-  };
   return (
     <>
       <h1>List Group</h1>
-      {getMessage}
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
+        {items.map((item, index) => (
           <li
-            className="list-group-item"
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
             key={item}
-            onClick={() => console.log(item)}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
           >
             {item}
           </li>
